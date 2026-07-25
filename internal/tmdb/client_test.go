acf56423eb6bcc462f5c26bed03d35e83d0af723
MMA <58857539+TheGeeKing@users.noreply.github.com>
2026-07-25 22:23:34 +0200

    test: cover transient TMDB retries

--- internal/tmdb/client_test.go
+++ internal/tmdb/client_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -54,5 +55,26 @@ func TestClientErrorAndCancellation(t *testing.T) {
 	cancel()
 	if _, err := client.Translations(ctx); err != context.Canceled {
 		t.Fatalf("canceled request error = %v", err)
+	}
+}
+
+func TestClientRetriesTransientFailure(t *testing.T) {
+	var requests atomic.Int32
+	server := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, _ *http.Request) {
+		if requests.Add(1) < 2 {
+			writer.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+		_, _ = writer.Write([]byte(`["en-US","fr-FR"]`))
+	}))
+	defer server.Close()
+
+	client := NewWithHTTPClient("token", server.URL, server.Client())
+	languages, err := client.Translations(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(languages) != 2 || requests.Load() != 2 {
+		t.Fatalf("translations=%v requests=%d", languages, requests.Load())
 	}
 }
